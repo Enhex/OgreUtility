14e016a3ce5925e9719a6df626e92362ebc62542
Enhex <enhex93@gmail.com>
2014-08-22 21:36:23 +0300

    Initial commit. Includes polygon level raycasting.

---
+++ include/OgreUtility.hpp
@@ -0,0 +1,8 @@
+#ifndef OGRE_UTILITY_OGRE_UTILITY_HPP
+#define OGRE_UTILITY_OGRE_UTILITY_HPP
+
+// Include all the OgreUtility headers
+#include "PolygonRaycast.hpp"
+
+
+#endif//OGRE_UTILITY_OGRE_UTILITY_HPP
---
+++ include/PolygonRaycast.hpp
@@ -0,0 +1,23 @@
+#ifndef OGRE_UTILITY_POLYGON_RAYCAST_HPP
+#define OGRE_UTILITY_POLYGON_RAYCAST_HPP
+
+#include "Ogre.h"
+
+namespace OgreUtility
+{
+	// raycast from a point in to the scene.
+	// returns success or failure.
+	// on success the point is returned in the result.
+	bool RaycastFromPoint(Ogre::RaySceneQuery* raySceneQuery, const Ogre::Ray& ray, Ogre::Vector3 &result);
+
+	// Get the mesh information for the given entity.
+	void GetMeshInformation(const Ogre::Entity* entity, size_t& vertex_count, Ogre::Vector3*& vertices, size_t& index_count, unsigned long*& indices, const Ogre::Vector3& position, const Ogre::Quaternion& orient, const Ogre::Vector3& scale);
+	
+	// Get the mesh information for the given mesh.
+	void GetMeshInformation(const Ogre::MeshPtr mesh, size_t &vertex_count, Ogre::Vector3*& vertices, size_t& index_count, unsigned long*& indices, const Ogre::Vector3& position, const Ogre::Quaternion& orient, const Ogre::Vector3& scale);
+
+	// Get the mesh information for the given manual object.
+	void GetMeshInformation(const Ogre::ManualObject* manual, size_t& vertex_count, Ogre::Vector3*& vertices, size_t& index_count, unsigned long*& indices, const Ogre::Vector3& position, const Ogre::Quaternion& orient, const Ogre::Vector3& scale);
+}
+
+#endif//OGRE_UTILITY_POLYGON_RAYCAST_HPP
